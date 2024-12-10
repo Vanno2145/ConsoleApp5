d0766dd13b7875b29b1e00d3d22cee9d97bee99e
ivanm <ivanm@DESKTOP-F33GBDN>
2024-12-10 11:17:23 +0300

    sdf

---
+++ ConsoleApp5/FileName.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+interface IStore
+{
+    void DisplayInfo(); 
+}
+
+class Store : IStore, IDisposable
+{
+    
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string StoreType { get; set; }
+
+   
+    public Store(string name, string address, string storeType)
+    {
+        Name = name;
+        Address = address;
+        StoreType = storeType;
+    }
+
+    
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Название магазина: {Name}");
+        Console.WriteLine($"Адрес магазина: {Address}");
+        Console.WriteLine($"Тип магазина: {StoreType}");
+    }
+
+    
+    public void Dispose()
+    {
+       
+        Console.WriteLine($"Ресурсы магазина '{Name}' освобождены.");
+    }
+    ~Store()
+    {
+        Console.WriteLine($"Объект магазина '{Name}' уничтожен.");
+    }
+}
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        
+        using (Store store = new Store("Магазин Одежды", "Москва, ул. Ленина, 1", "Одежда"))
+        {
+            store.DisplayInfo();
+            
+        }
+
+      
+        Console.WriteLine("Программа завершена.");
+        Console.ReadLine();
+    }
+}
